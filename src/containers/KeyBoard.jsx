ad8f6bdee01c11f3fde6fdcf6c7c147a5229952d
Charise Walraven <cherri.walraven@gmail.com>
2018-07-06 19:11:19 +0200

    fix overlapping elements

--- src/containers/KeyBoard.jsx
+++ src/containers/KeyBoard.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Button } from '@material-ui/core'
+import './KeyBoard.css'
 
 const KeyBoard = ({ ...props }) => {
   const { qwerty, handleClick } = props
   const qwertyKeys = [["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"], ["a", "s", "d", "f", "g", "h", "j", "k", "l"], ["z", "x", "c", "v", "b", "n", "m"]]
   if (qwerty) {
     return (
-      <div>
+      <div className={'KeyBoard'}>
         <div>
           {
             qwertyKeys[0].map((k, i) => <Button key={'button-0-'+ i} onClick={_ => handleClick(k)}>{k}</Button>)
