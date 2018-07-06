ed26507c933d46d047eec446b16d8d9ff438d1b7
Charise Walraven <cherri.walraven@gmail.com>
2018-07-06 18:59:10 +0200

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,34 +1,13 @@
 import React from 'react'
-import { Typography, withStyles, Grid, Paper } from '@material-ui/core'
-import PropTypes from 'prop-types';
+import { Typography } from '@material-ui/core'
 import './Footer.css'
 
-
-const styles = theme => ({
-  root: theme.mixins.gutters({
-    display: 'flexGrow',
-    padding: 16,
-  }),
-})
-
 const Footer = ({ ...props }) => {
-  const { classes, theme } = props
   return (
-    <Paper elevation={4} className={classes.root} id='Footer'>
-      <Grid container className={classes.root} spacing={theme.spacing.unit * 2}>
-        <Grid item xs={12}>
-          <Grid container alignItems={'flex-end'}
-            spacing={theme.spacing.unit * 2} justify='center'>
-            <Typography component='a' variant='subheading' href='#'>Copyright Charise Walraven 2018</Typography>
-          </Grid>
-        </Grid>
-      </Grid>
-    </Paper>
+    <footer>
+      <Typography component='a' variant='subheading' href='https://charise.info'>Copyright Charise Walraven 2018</Typography>  
+    </footer>
   )
 }
 
-Footer.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(Footer)
+export default Footer
